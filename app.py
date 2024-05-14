6a05f978b3ff7f4c7e8490827864a4ddee76c331
areebahmed575 <areebahmed575@gmail.com>
2024-05-14 23:42:44 +0500

    Medical Chatbot almost done

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, jsonify, request
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.llms import OpenAI
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAI
+from src.helper import load_pdf, text_split
+
+app = Flask(__name__)
+
+load_dotenv()
+
+
+os.environ["OPENAI_API_KEY"]=""
+
+embedding=OpenAIEmbeddings()
+
+extracted_data = load_pdf("data/")
+text_chunks = text_split(extracted_data)
+
+persist_directory = 'db'
+vectordb = Chroma.from_documents(documents=text_chunks,embedding=embedding,persist_directory=persist_directory)
+vectordb.persist() # persiste the db to disk
+vectordb = None
+# Now we can load the persisted database from disk, and use it as normal.
+vectordb = Chroma(persist_directory=persist_directory,embedding_function=embedding)
+
+PROMPT=PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+
+retriever = vectordb.as_retriever(search_kwargs={"k": 2})
+chain_type_kwargs={"prompt": PROMPT}
+
+
+
+
+# create the chain to answer questions
+qa_chain = RetrievalQA.from_chain_type(llm=OpenAI(),
+                                  chain_type="stuff",
+                                  retriever=retriever,
+                                  return_source_documents=True)
+
+
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    result=qa_chain({"query": input})
+    print("Response : ", result["result"])
+    return str(result["result"])
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port= 8080, debug= True)
---
+++ store_index.py
@@ -0,0 +1,26 @@
+from src.helper import load_pdf, text_split
+from dotenv import load_dotenv
+import os
+from langchain_openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain_community.vectorstores import Chroma
+from langchain import embeddings
+
+load_dotenv()
+
+
+os.environ["OPENAI_API_KEY"]=""
+
+embedding=OpenAIEmbeddings()
+extracted_data = load_pdf("data/")
+text_chunks = text_split(extracted_data)
+
+
+
+
+persist_directory = 'db'
+vectordb = Chroma.from_documents(documents=text_chunks,embedding=embedding,persist_directory=persist_directory)
+vectordb.persist() # persiste the db to disk
+vectordb = None
+# Now we can load the persisted database from disk, and use it as normal.
+vectordb = Chroma(persist_directory=persist_directory,embedding_function=embedding)
